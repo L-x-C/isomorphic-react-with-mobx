fee35eaf5a5479d939968466e6a56bbc59e2c28d
L-x-C <lxc.neal@gmail.com>
2016-12-05 17:40:19 +0800

    refine style

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -6,8 +6,8 @@ export default class Menu extends Component {
   render() {
     return (
       <div className="menu">
-        <Link to="/test2" className="menu-link" activeClassName="active">test2</Link>
-        <Link to="/test" className="menu-link" activeClassName="active">test1</Link>
+        <Link to="/test2" className="menu-link" activeClassName="active">page2</Link>
+        <Link to="/test" className="menu-link" activeClassName="active">page1</Link>
       </div>
     );
   }
